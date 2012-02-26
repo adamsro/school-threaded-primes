95b682add58d32a9a1079c8cec9c0d47b377b3c9
Marshall Adams <rma.kona@gmail.com>
2012-02-25 16:48:15 -0800

    fixed testnum = 1, runs 429496729 in 61 sec

--- primes.cpp
+++ primes.cpp
@@ -38,10 +38,9 @@ int hits = 1;
 int finished_threads; /* indicates that all threads are complete */
 
 void *mark_not_prime(void *t) {
-    long my_id = (long) t;
+    //long my_id = (long) t;
     unsigned long mom;
-    unsigned long testnum;
-    printf("Starting mark_not_prime(): thread %ld\n", my_id);
+    unsigned long testnum = 1;
     while ((testnum += 2) <= max) {
         if (!TEST(bitmap, testnum)) {
             ++hits;
@@ -51,14 +50,15 @@ void *mark_not_prime(void *t) {
         }
     }
     ++finished_threads; /* indicate thread is complete */
-    printf("Ending mark_not_prime(): thread %ld\n", my_id);
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
     pthread_attr_t attr;
+    time_t start;
+    time_t theend;
 
-    (argc > 1) ? num_threads = atol(argv[1]) : num_threads = 3;
+    (argc > 1) ? num_threads = atol(argv[1]) : num_threads = 8;
     // at 2^32 or set to INT_MAX
     (argc > 2) ? max = atol(argv[2]) : max = 4294967296;
     //upperlim = sqrt(max);
@@ -75,26 +75,33 @@ int main(int argc, char *argv[]) {
     /* For portability, explicitly create threads in a joinable state */
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    std::cout << "Searching for " << max << " primes. \n";
+
+    start = clock();
     for (int i = 0; i < num_threads; ++i) {
         pthread_create(&threads[i], &attr, mark_not_prime, (void *) thread_ids[i]);
     }
-
     while (finished_threads < num_threads) {
         ; /* until all threads are complete */
     }
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
+    theend = clock();
     printf("Main(): Waited on %d threads. Done.\n", num_threads);
+    std::cout << "found " << hits << " in ";
+    std::cout << (((double) (theend - start)) / CLOCKS_PER_SEC);
+    std::cout << " seconds" << std::endl;
 
     /* This alg speeds things up by not checking even numbers,
-     * 2 however is an exception so it must be added to the output. */
+     * 2 however is an exception so it must be added to the output.
     printf("%d\t", 1);
     printf("%d\t", 2);
     unsigned long k = 1;
     while ((k += 2) < max)
         if (!TEST(bitmap, k)) std::cout << k << "\t";
-
+     */
     /* Clean up and exit */
     pthread_attr_destroy(&attr);
     pthread_exit(NULL);
